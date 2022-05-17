30162163c47277ed86c3e72bca4309aa17beafaa
ahhridoy <ahhridoyy28@gmail.com>
2022-05-18 02:22:56 +0600

    services batch sound added

--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -1,10 +1,9 @@
 import React from "react";
-import {  Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import FaqComponent from "../../components/FaqComponent";
 import "../../styles/pages/_services.scss";
 
 const Services = () => {
-
   return (
     <main>
       <section id="portfolio">
@@ -36,12 +35,45 @@ const Services = () => {
       </section>
       <section id="looking-for">
         <Container>
-          <div>
+          <div className="heading">
             <h1>What are you looking for?</h1>
           </div>
-          <div>
-             <FaqComponent />
+          <div className="accordion">
+            <FaqComponent />
           </div>
+        </Container>
+      </section>
+      <section id="we-writing">
+        <Container className="text-box">
+          <h1>We Writing</h1>
+          <p>
+            At UK Essays, we're proud to know exactly what goes into making an
+            awesome essay. We've dedicated over 14 years towards becoming
+            experts in all things academic – from essays, assignments and
+            coursework, through to dissertations, exams, reports and
+            reflections. While others come and go, UK Essays has grown and
+            developed into one of the world's leading academic support
+            companies. Got more questions about our services?
+          </p>
+          <Button variant="danger" className="text-light">
+            Check Out Our <b>Samples</b>
+          </Button>
+        </Container>
+      </section>
+      <section id="essay-heros">
+        <Container className="text-box">
+          <h1>We Writing</h1>
+          <p>
+            We use a team of experts and academics to build your order and
+            create a fantastic, unique and exciting piece of work every time. We
+            only use highly qualified writers, including lecturers, teachers and
+            PhD holders. Our in-house teams are on hand to support you with
+            everything from placing your order through to ensuring your order is
+            in perfect condition before being sent to you.
+          </p>
+          <Button variant="primary" className="text-light">
+            Discover More <b>About UKEssays</b>
+          </Button>
         </Container>
       </section>
     </main>
